c1ce69be4bbac793cf19f9603a8353140afa5049
pangeding <3532341261@qq.com>
2026-03-31 17:37:32 +0800

    feat: basic flow structure

--- src/main.py
+++ src/main.py
@@ -1,8 +1,25 @@
 from dotenv import load_dotenv
 
+from langgraph.graph import END, MessagesState, StateGraph
+from langchain_core.messages import HumanMessage
+
 load_dotenv()
 
+LAST = -1
 
+flow = StateGraph(MessagesState)
+
+app = flow.compile()
 
 if __name__ == "__main__":
     print("Hello ReAct with LangGraph")
+    res = app.invoke(
+        {
+            "messages": [
+                HumanMessage(
+                    content="What is the weather in Beijing? List it and triple it."
+                )
+            ]
+        }
+    )
+    print(res["messages"][LAST].content)
